756ebc2d1f70adbedb62164fbf1234a588d0766f
Cot1ch <vsevolod310306@mail.ru>
2025-04-26 18:14:27 +0300

    поправлен класс program

--- RecsApp/Program.cs
+++ RecsApp/Program.cs
@@ -13,7 +13,7 @@ namespace RecsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new Form1());
         }
     }
 }
